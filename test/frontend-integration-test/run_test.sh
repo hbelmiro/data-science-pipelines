7133729e72ded16605ad521356f8b7110f736bb1
hbelmiro <helber.belmiro@gmail.com>
2024-06-08 11:07:31 -0300

    e2e - WIP - added frontend-it
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- test/frontend-integration-test/run_test.sh
+++ test/frontend-integration-test/run_test.sh
@@ -42,14 +42,8 @@ function parse_bool {
 
 while [[ "$1" != "" ]]; do
     case $1 in
-             --results-gcs-dir )shift
-                                RESULTS_GCS_DIR=$1
-                                ;;
              --namespace )      shift
                                 NAMESPACE=$1
-                                ;;
-             --remote-run )     shift
-                                REMOTE_RUN=$(parse_bool "${1:-}")
                                 ;;
              -h | --help )      usage
                                 exit
@@ -60,50 +54,30 @@ while [[ "$1" != "" ]]; do
     shift
 done
 
-if [[ "$REMOTE_RUN" != "true" ]]; then
-  if [[ -z "$RESULTS_GCS_DIR" ]]; then
-      usage
-      exit 1
-  fi
-  if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
-    gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
-  fi
-fi
-
 npm install
 
-if [[ "$REMOTE_RUN" != "true" ]]; then
-  function clean_up() {
-    set +e
+function clean_up() {
+set +e
 
-    echo "Stopping background jobs..."
-    kill -15 %1
-    kill -15 %2
-  }
-  trap clean_up EXIT SIGINT SIGTERM
+echo "Stopping background jobs..."
+kill -15 %1
+kill -15 %2
+}
+trap clean_up EXIT SIGINT SIGTERM
 
-  # Port forward the UI so tests can work against localhost
-  POD=`kubectl get pods -n ${NAMESPACE} -l app=ml-pipeline-ui -o jsonpath='{.items[0].metadata.name}'`
-  kubectl port-forward -n ${NAMESPACE} ${POD} 3000:3000 &
-fi
+# Port forward the UI so tests can work against localhost
+POD=`kubectl get pods -n ${NAMESPACE} -l app=ml-pipeline-ui -o jsonpath='{.items[0].metadata.name}'`
+kubectl port-forward -n ${NAMESPACE} ${POD} 3000:3000 &
 
 # Run Selenium server
 /opt/bin/entry_point.sh &
 ./node_modules/.bin/wait-port 127.0.0.1:4444 -t 20000
 ./node_modules/.bin/wait-port 127.0.0.1:3000 -t 20000
 
-export PIPELINE_OUTPUT=${RESULTS_GCS_DIR}/pipeline_output
 # Don't exit early if 'npm test' fails
 set +e
 npm test
 TEST_EXIT_CODE=$?
 set -e
-
-JUNIT_TEST_RESULT=junit_FrontendIntegrationTestOutput.xml
-
-if [[ "$REMOTE_RUN" != "true" ]]; then
-  echo "Copy test result to GCS ${RESULTS_GCS_DIR}/${JUNIT_TEST_RESULT}"
-  gsutil cp ${JUNIT_TEST_RESULT} ${RESULTS_GCS_DIR}/${JUNIT_TEST_RESULT}
-fi
 
 exit $TEST_EXIT_CODE
