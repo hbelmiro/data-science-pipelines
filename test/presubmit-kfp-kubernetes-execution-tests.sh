79f6f4b080273795d12f52ab2f5b81c75b6c7e6f
hbelmiro <helber.belmiro@gmail.com>
2024-06-14 16:32:45 -0300

    kfp-kubernetes-execution-tests-gha - WIP
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- test/presubmit-kfp-kubernetes-execution-tests.sh
+++ test/presubmit-kfp-kubernetes-execution-tests.sh
@@ -13,29 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-source_root=$(pwd)
+echo "$0 should never be called"
+exit 1
 
-python3 -m pip install --upgrade pip
-python3 -m pip install $source_root/sdk/python
-apt-get update && apt-get install -y protobuf-compiler
-# install kfp-pipeline-spec from source
-pushd api
-make clean python
-popd
-python3 -m pip install api/v2alpha1/python
-
-# generate kfp-kubernetes proto files from source
-pushd "$source_root/kubernetes_platform"
-make clean python
-popd
-
-# install kfp-kubernetes from source
-# rust needed for transitive deps in dev extras on Python:3.12
-apt-get install rustc -y
-pip install -e $source_root/kubernetes_platform/python[dev]
-
-pip install -r $source_root/test/kfp-kubernetes-execution-tests/requirements.txt
-
-export KFP_ENDPOINT="http://localhost:8888"
-export TIMEOUT_SECONDS=2700
-pytest $source_root/test/kfp-kubernetes-execution-tests/sdk_execution_tests.py --asyncio-task-timeout $TIMEOUT_SECONDS
+# TODO: Helber - remove this file
